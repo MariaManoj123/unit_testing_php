54153b6c71148d281d0db0be10ae25f64e169268
MariaManoj123 <166786203+MariaManoj123@users.noreply.github.com>
2024-04-15 11:36:54 +0530

    Add files via upload

---
+++ test case for addition.php
@@ -0,0 +1,16 @@
+<?php 
+
+require 'math.php';
+
+use PHPUnit\Framework\TestCase;
+
+class MathTest extends TestCase {
+    public function testAdd() {
+        $a = 5;
+        $b = 3;
+        $expectedResult = 8;
+        $result = add($a, $b);
+        $this->assertEquals($expectedResult, $result);
+    }
+}
+?>
